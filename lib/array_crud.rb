fab67158dcf09f81795a2f046ee280555fc84261
Paris Dolfman <parisdolfman@gmail.com>
2018-04-05 23:09:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  round_table = ["Mama", "Jason", "Papa Panda", "Baby Panda"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(babies, element)
+  babies = ["Tristin" "Draco", "Ashes", "Kingston"]
+  babies << "arrays!"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(babies, element)
+  babies = ["wow" "Draco", "Ashes", "Kingston"]
+  babies.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(babies)
+  babies = ["Tristin" "Draco", "Ashes", "arrays!"]
+  draco = "Draco"
+  draco = babies.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(babies)
+  babies = ["arrays!" "Draco", "Ashes", "Kingston"]
+  tristin = "arrays!"
+  tristin = babies.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(babies, index_number)
+    babies = ["arrays!" "Draco", "Ashes", "Kingston"]
+    babies[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
