c8ac50470ee6d8ba02c76a25276922f209d59046
Paris Dolfman <parisdolfman@gmail.com>
2018-04-05 23:10:18 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_end_of_array(babies)
 end
 
 def remove_element_from_start_of_array(babies)
-  babies = ["arrays!" "Draco", "Ashes", "Kingston"]
+  babies = ["arrays!"], "Draco", "Ashes", "Kingston"]
   tristin = "arrays!"
   tristin = babies.shift
 end
